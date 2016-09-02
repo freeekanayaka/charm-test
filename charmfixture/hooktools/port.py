ac1f809fd677b12936b4885357dceac34c7064f1
Free Ekanayaka <free.ekanayaka@gmail.com>
2016-09-02 11:28:46 +0000

    Add open-port hook tool

---
+++ charmfixture/hooktools/port.py
@@ -0,0 +1,31 @@
+import os
+
+
+class PortData():
+    """Access ports opened by the fake open-port hook tool."""
+
+    def __init__(self):
+        """
+        @param path: Path to the backend log file.
+        """
+        with open(self.path(), "w") as fd:
+            fd.write("")
+
+    @classmethod
+    def path(cls):
+        return os.path.join(os.environ["CHARM_DIR"], ".open-port.data")
+
+    def __iter__(self):
+        with open(self.path()) as fd:
+            for line in fd.readlines():
+                yield line.strip()
+
+    def __getitem__(self, index):
+        return list(self)[index]
+
+
+class PortTool():
+
+    def run(self, args):
+        with open(PortData.path(), "w") as fd:
+            fd.write(args[0])
