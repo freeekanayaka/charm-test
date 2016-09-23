f5224b606ddb23dd4243f8a9eea1f8840c00058e
Free Ekanayaka <free@ekanayaka.io>
2016-09-23 07:20:38 +0000

    Increase test coverage

--- charmfixtures/hooktools/fixture.py
+++ charmfixtures/hooktools/fixture.py
@@ -16,10 +16,6 @@ BIN = os.path.join(os.path.dirname(__file__), "bin")
 class HookTools(Fixture):
     """Provide fake versions of juju hook tools."""
 
-    def log(self):
-        with open(self._tools_dir.join(".log")) as fd:
-            return [line.strip() for line in fd.readlines()]
-
     def _setUp(self):
         self._charm_dir = self.useFixture(TempDir())
 
