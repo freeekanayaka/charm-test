37b148f3cb91d9cf9ee5d3f32ea2d5903fa596e4
Free Ekanayaka <free.ekanayaka@gmail.com>
2016-09-23 08:12:46 +0000

    Clear hookenv cache

--- charmfixtures/testing.py
+++ charmfixtures/testing.py
@@ -1,9 +1,14 @@
-from testtools import TestCase
+from testtools import (
+    TestCase,
+    try_import,
+)
 
 from charmfixtures.filesystem import Filesystem
 from charmfixtures.users import Users
 from charmfixtures.groups import Groups
 from charmfixtures.hooktools.fixture import HookTools
+
+hookenv = try_import("from charmhelpers.core.hookenv")
 
 
 class CharmTest(TestCase):
@@ -14,3 +19,5 @@ class CharmTest(TestCase):
         self.users = self.useFixture(Users())
         self.groups = self.useFixture(Groups())
         self.hooktools = self.useFixture(HookTools())
+        # If charmhelpers is around, clear its config cache
+        hookenv and hookenv.cache.clear()
