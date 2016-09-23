e624f94c746d96ae3d4763b83626567d0f2f6660
Free Ekanayaka <free.ekanayaka@gmail.com>
2016-09-23 07:32:27 +0000

    More assertions

--- charmfixtures/tests/test_testing.py
+++ charmfixtures/tests/test_testing.py
@@ -5,3 +5,6 @@ class MyCharmTest(CharmTest):
 
     def test_fixtures(self):
         self.assertTrue(self.filesystem)
+        self.assertTrue(self.groups)
+        self.assertTrue(self.users)
+        self.assertTrue(self.hooktools)
