ed3ac75612b79320822bea029d3436298e4b1ede
Free Ekanayaka <free@ekanayaka.io>
2016-10-05 11:45:27 +0200

    For systemd

--- charmtest/__init__.py
+++ charmtest/__init__.py
@@ -1,10 +1,18 @@
 import platform
 
+from testtools import try_import
+
 from .testing import CharmTest
 
 # XXX Force 'ubuntu' as platform, since charmhelpers.core.host doesn't like
 # 'debian' (which is what you get on Travis).
 platform.linux_distribution = lambda: ('Ubuntu', '16.04', 'xenial')
+
+# XXX Force systemd as init system, since Travis doesn't yet support xenial.
+host = try_import("charmhelpers.core.hookenv")
+
+if host:
+    host.init_is_systemd = lambda: True
 
 
 __all__ = [
