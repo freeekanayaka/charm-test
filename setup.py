ed856564ac264be0c9dc37a5da97e62bec81ce2f
Free Ekanayaka <free.ekanayaka@gmail.com>
2016-10-05 06:27:27 +0000

    Rename URLs as well

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # Copyright 2016 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
-"""Distutils installer for charm-fixtures."""
+"""Distutils installer for charm-test."""
 
 from setuptools import setup, find_packages
 
@@ -29,5 +29,5 @@ setup(
     description="Create fake juju hook tools for charm unit testing",
     license="AGPL",
     keywords="fixtures juju charm",
-    url="https://github.com/freeekanayaka/charm-fixtures",
+    url="https://github.com/freeekanayaka/charm-test",
 )
