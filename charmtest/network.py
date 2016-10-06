e07eb1a0bea938049534f4e9869f0b5353802dd0
Free Ekanayaka <free.ekanayaka@gmail.com>
2016-10-06 03:54:36 +0000

    Add network/wget support

---
+++ charmtest/network.py
@@ -0,0 +1,25 @@
+import io
+import argparse
+
+
+class Wget(object):
+
+    name = "wget"
+
+    def __init__(self, network):
+        self._network = network
+
+    def __call__(self, proc_args):
+        parser = argparse.ArgumentParser()
+        parser.add_argument("url")
+        parser.add_argument("-O", dest="output")
+        parser.add_argument("-q", dest="quite")
+        args = parser.parse_args(proc_args["args"][1:])
+        content = self._network[args.url]
+        result = {}
+        if args.output == "-":
+            result["stdout"] = io.BytesIO(content)
+        else:
+            with open(args.output, "wb") as fd:
+                fd.write(content)
+        return result
